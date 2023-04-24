caf04eea1a19dd941d2d68d91ca3197cbb18431e
Fabio Santos <fabioov@hotmail.com>
2023-04-24 15:02:12 -0300

    Updated Search Box and New Header

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -20,9 +20,10 @@ export class DataService {
   getNews(symbol: string): Observable<NewsData> {
     debugger
     return this.http.get<NewsData>(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${symbol}&apikey=8AI8BXTX0FG34321`);
+    
   }
   getCompany(company: string): Observable<SymbolSearch> {
-    debugger
+
     return this.http.get<SymbolSearch>(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${company}&apikey=8AI8BXTX0FG34321`);
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import * as moment from 'moment';
+import { ViewChild, ElementRef } from '@angular/core';
 
 type Header = {
   '2. Symbol': string;
@@ -10,14 +11,14 @@ type Header = {
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
+  @ViewChild('inputField', { static: false }) inputField!: ElementRef;
   title = 'stock-app';
   header: Header;
-  data: {[key: string]: any};
-  companySearch: {[key: string]: any}
+  data: { [key: string]: any };
+  companySearch: { [key: string]: any };
   daily: any;
   date: string;
   dateFormatted: string;
@@ -30,38 +31,61 @@ export class HomeComponent implements OnInit {
     public dataService: DataService // add SymbolSearchService dependency
   ) {}
 
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-
-   }
-
-   onButtonClick(item: any) {
-    debugger
+  onButtonClick(item: any) {
     // Here, you can use the value of the symbol variable
-    this.symbol = item + '.SA';
-    // You can also pass it to a method in your DataService to fetch data for that symbol
-    this.dataService.getNews(this.symbol).subscribe((result: any) => {
-    
-      this.data = result['Time Series (Daily)'];
-      const date = '2022-11-23';
-      this.daily = this.data[date];
-      this.date = moment(date).format('DD-MM-YYYY');
-      
-      debugger
-      this.header = result['Meta Data'];
-    });
+    const inputValue = this.inputField.nativeElement.value.replace(/\s/g, '');
+    const compTick = inputValue.split("-");
+
+    if (inputValue) {
+      this.symbol = compTick[0];
+      // You can also pass it to a method in your DataService to fetch data for that symbol
+      this.dataService.getNews(this.symbol).subscribe((result: any) => {
+        debugger
+        this.handleResult(result);
+      });
+    }
+  }
+
+  handleResult(result: any) {
+    this.data = result['Time Series (Daily)'];
+        const date = '2023-03-17';
+        this.daily = this.data[date];
+        this.date = moment(date).format('DD-MM-YYYY');
+        this.header = result['Meta Data'];
+        console.log('This is the data: ', this.data);
+        console.log('This is the header: ', this.header);
+        if (this.daily === undefined ) {
+          console.log(`This set of data does not contain the date ${this.date}!`);
+        }else {
+        console.log(`This is the daily:, ${this.daily}`);
+        }
+        debugger
   }
 
   onKeyUp(companyName: any) {
-debugger
-    this.searchQuery = companyName.target.value;
+    debugger;
+    if (!companyName.target.value) {
+      console.log('Clearing companyTicker array');
+      this.companyTicker = [];
+    } else {
+      console.log('Fetching company data for:', companyName.target.value);
+      this.searchQuery = companyName.target.value;
 
-    this.dataService.getCompany(this.searchQuery).subscribe((search: any) => {
-debugger
-      this.companyTicker = search['bestMatches'];
-
-    });
+      this.dataService.getCompany(this.searchQuery).subscribe((search: any) => {
+        console.log('Received data:', search);
+        this.companyTicker = search['bestMatches'];
+      });
+    }
   }
 
-  
+  onResultBoxClick(item: any) {
+    console.log(item);
+    const searchInput = document.querySelector(
+      '.searchInput input'
+    ) as HTMLInputElement;
+    searchInput.value = `${item['1. symbol']} - ${item['2. name']}`;
+    this.isSearchActive = false;
+  }
 }
