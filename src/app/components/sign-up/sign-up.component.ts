879b4be9bc17a6227d8ad2f1b2f17100565c324a
Fabio Santos <fabioov@hotmail.com>
2023-05-13 00:15:20 -0300

    SignUp almost ready

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -27,20 +27,61 @@ export function passwordsMatchValidator(): ValidatorFn {
   };
 }
 
+export function passwordFormatValidator(passwordErrors: string[]): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+
+    debugger
+
+    // const password = control.get('password')?.value;
+    const password = control.value;
+
+    console.log('Password:', password); // Log the value of the password field
+    const errors: ValidationErrors = {};
+
+    if (password && password.length < 8) {
+
+      errors['minLength'] = 'Password must be at least 8 characters long.';
+      passwordErrors.push('Password must be at least 8 characters long.');
+    }
+
+    if (password &&  !/\d/.test(password)) {
+      errors['hasNumber'] = 'Password must contain at least one number.';
+      passwordErrors.push('Password must contain at least one number.');
+    }
+
+    if (password &&  (!/[a-z]/.test(password) || !/[A-Z]/.test(password))) {
+      errors['hasLetter'] = 'Password must contain both lowercase and uppercase letters.';
+      passwordErrors.push('Password must contain both lowercase and uppercase letters.');
+    }
+
+    if (password &&  !/\W/.test(password)) {
+      errors['hasSymbol'] = 'Password must contain at least one symbol.';
+      passwordErrors.push('Password must contain at least one symbol.');
+    }
+
+    if (Object.keys(errors).length > 0) {
+      console.log('Validation Errors:', errors); // Log the validation errors
+      return errors;
+    }
+
+    return null;
+  };
+}
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
+  passwordErrors: string[] = [];
   signUpForm = new FormGroup(
     {
       name: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.email, Validators.required]),
-      password: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, passwordFormatValidator(this.passwordErrors)]),
       confirmPassword: new FormControl('', Validators.required),
     },
-    { validators: passwordsMatchValidator() }
+    { validators: [passwordsMatchValidator(), passwordFormatValidator(this.passwordErrors)] }
   );
 
   constructor(
@@ -49,7 +90,9 @@ export class SignUpComponent implements OnInit {
     private router: Router
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    
+  }
 
   get name() {
     return this.signUpForm.get('name');
@@ -79,7 +122,9 @@ export class SignUpComponent implements OnInit {
           this.toast.success('User created successfully!', 'Congratulations!');
           this.router.navigate(['/home']);
         }),
+        
         catchError((error) => {
+          debugger;
           this.toast.error('User not created. Try again', 'Check the data!');
           return of(null);
         })
